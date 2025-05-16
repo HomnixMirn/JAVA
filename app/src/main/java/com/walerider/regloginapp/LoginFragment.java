1238733739e909dc0f0ac04e21158c0ea5823e50
=Walerider <walerider.xbox@yandex.ru>
2025-05-16 23:59:33 +0400

    init

--- app/src/main/java/com/walerider/regloginapp/LoginFragment.java
+++ app/src/main/java/com/walerider/regloginapp/LoginFragment.java
@@ -3,6 +3,8 @@ package com.walerider.regloginapp;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +12,7 @@ import android.view.ViewGroup;
 
 public class LoginFragment extends Fragment {
 
+    private NavController navController;
     public LoginFragment() {
     }
     /*TODO 2 поля при вводе: Логин, пароль.Сделать 2 поля EditText в xml
@@ -31,6 +34,8 @@ public class LoginFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_login, container, false);
+        navController = Navigation.findNavController(view);
+        navController.navigate(R.id.registerFragment);
         return view;
     }
 }
